86f3ae1f1426f2dd2fe30160d40d1c829eab0403
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-07-02 04:12:29 -0500

    Server & logs env

--- config/env.js
+++ config/env.js
@@ -1,6 +1,12 @@
 const dotenv = require("dotenv");
 dotenv.config();
 
+if (dotenv.error) {
+  throw dotenv.error;
+}
+
+console.log("Successfull: ", dotenv.parsed);
+
 //Environments of the project
 module.exports = {
   url_mongo: process.env.MONGO_DB,

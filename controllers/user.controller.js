8184bf8d2f8a74b5bd6bf8245d032b8071c3ee43
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-07-02 18:15:00 -0500

    controller user & model

---
+++ controllers/user.controller.js
@@ -0,0 +1,86 @@
+//Model
+const User = require("../models/user.model");
+
+const renderTest = (req, res) => {
+  res.send("<h1 style='color:red'>Test</h1>");
+};
+
+const renderCreateOne = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const user = new User({ username });
+    user.password = await encryptPassword(password);
+    await user.save();
+    if (!user) {
+      throw new Error("User has not been saved");
+    }
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+  }
+};
+const renderCreateMany = async (req, res) => {};
+
+const renderReadOne = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (!user) {
+      throw new Error("User no exist");
+    }
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+  }
+};
+const renderReadMany = async (req, res) => {
+  /* 
+      /user/all?limit=0&where[]=student&where[]=root&sort=-_id
+      
+      Limit reduces the number of objects returned, 0 returns all.
+      Where conditions objects based on type_user.
+      Sort sorts the ascending (+) or descending (-) objects indicating the field by which to sort.
+    */
+
+  try {
+    //Query from req
+    let { limit, where, sort } = req.query;
+    //Default values
+    if (!limit) {
+      limit = 0;
+    }
+    if (!where || where.length < 1) {
+      where = await User.schema.path("type_user").enumValues; //Get default enum from model
+    }
+    if (!sort) {
+      sort = "+_id"; //Default sort
+    }
+    //Find by query
+    const user = await User.find()
+      .limit(parseInt(limit))
+      .where("type_user", where)
+      .sort(sort);
+    //Return a object
+    res.send(user);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const renderUpdateOne = async (req, res) => {};
+const renderUpdateMany = async (req, res) => {};
+
+const renderDeleteOne = async (req, res) => {};
+const renderDeleteMany = async (req, res) => {};
+
+module.exports = {
+  renderTest,
+  renderCreateOne,
+  renderCreateMany,
+  renderReadOne,
+  renderReadMany,
+  renderUpdateOne,
+  renderUpdateMany,
+  renderDeleteOne,
+  renderDeleteMany,
+};
--- routes/user.route.js
+++ routes/user.route.js
@@ -2,60 +2,20 @@ const { Router } = require("express");
 const route = Router();
 const { encryptPassword } = require("../helpers/bpassword");
 
-//Model
-const User = require("../models/user.model");
+//Controller
+const {
+  renderTest,
+  renderReadMany,
+  renderReadOne,
+  renderCreateOne,
+} = require("../controllers/user.controller");
 
-route.get("/test", (req, res) => {
-  res.send("Test");
-});
+route.get("/test", renderTest);
 
-route.get("/users", async (req, res) => {
-  /* 
-    /users?limit=0&where[]=student&where[]=root&sort=-_id
-    
-    Limit reduces the number of objects returned, 0 returns all.
-    Where conditions objects based on type_user.
-    Sort sorts the ascending (+) or descending (-) objects indicating the field by which to sort.
-  */
+route.get("/user/all", renderReadMany);
 
-  try {
-    //Query from req
-    let { limit, where, sort } = req.query;
-    //Default values
-    if (!limit) {
-      limit = 0;
-    }
-    if (!where || where.length < 1) {
-      where = await User.schema.path("type_user").enumValues; //Get default enum from model
-    }
-    if (!sort) {
-      sort = "+_id"; //Default sort
-    }
-    //Find by query
-    const user = await User.find()
-      .limit(parseInt(limit))
-      .where("type_user", where)
-      .sort(sort);
-    //Return a object
-    res.send(user);
-  } catch (err) {
-    console.error(err);
-  }
-});
+route.get("/user/:id", renderReadOne);
 
-route.post("/user", async (req, res) => {
-  try {
-    const { username, password } = req.body;
-    const user = new User({ username });
-    user.password = await encryptPassword(password);
-    await user.save();
-    if (!user) {
-      throw new Error("User has not been saved");
-    }
-    res.send(user);
-  } catch (err) {
-    console.error(err);
-  }
-});
+route.post("/user/create", renderCreateOne);
 
 module.exports = route;

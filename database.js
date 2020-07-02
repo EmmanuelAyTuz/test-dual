155321faa630782a45909b82abe1897a12ccfb76
EmmanuelAyTuz <tuzemmanuel@gmail.com>
2020-07-02 05:49:29 -0500

    database.js connection

---
+++ database.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { mongo_db } = require("./config/env");
+
+mongoose
+  .connect(mongo_db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then((db) => {
+    console.log("Mongo DB is connected: ", db.connection.client.s.url);
+  })
+  .catch((err) => {
+    console.error("Mongo DB is NOT connected: ", err.message);
+  });
